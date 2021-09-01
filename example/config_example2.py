350cc77d00aa2af1a36442a42267a8ec16665eb6
swidler <swidler@gmail.com>
2021-09-01 11:49:50 +0300

    added second example

---
+++ example/config_example2.py
@@ -0,0 +1,30 @@
+# input files
+#dir = "inputdirectoryname/"
+dir = "example/"
+#indfile_main = dir + "indfilename.ind"  # IND file
+indfile_main = dir + "example_ind_file.ind"  # IND file
+#indfile_admix = dir + "famfilename.fam"  # FAM file
+indfile_admix = dir + "example_file.fam"  # FAM file
+#Qfile = dir + "Qfilename.K.Q"  # Q file
+Qfile = dir + "example_file.6.Q"  # Q file
+#Pfile = dir + "Pfilename.K.P"  # P file
+Pfile = dir + "example_file.6.P"  # P file
+#Gfile = dir + "genotypefilename.raw"  # G file
+Gfile = dir + "example_geno_file.raw"  # G file
+
+# output
+#out_dir = "outputdirectoryname/"
+out_dir = "example/"
+#outfile = out_dir + "outputfilename.txt"
+outfile = out_dir + "example2_results.txt"
+
+# user-supplied vars
+#target_pops = ["Target1","Target2",...,"TargetN"]
+target_pops = ["Som50Eng50","Som20Eng80"]
+#source_pops_var  = ["VaryingSource1","VaryingSource2",...,"VaryingSourceM"]
+source_pops_var  = ["Somali", "Spanish", "Iranian"]
+#source_pops_const = ["ConstantSource1","ConstantSource2",...,"ConstantSourceL"]
+source_pops_const = ["English","Jordanian"]
+num_reps = 1000  # number of repetitions desired for bootstrap in standard error estimation
+num_reps_pval = 10000  # number of repetitions desired for bootstrap in empirical p value calculations
+qval = 0.01  # threshold of values of ADMIXTURE ancestral populations to be considered in the bootstrap
